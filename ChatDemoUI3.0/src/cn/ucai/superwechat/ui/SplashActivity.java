90ed2e76ed0d5868b1d29a975234920f427e15a7
xieqiangdiao <765648978@qq.com>
2016-11-05 11:20:18 +0800

    登录成功闪屏获取数据包存到内存中

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/SplashActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/SplashActivity.java
@@ -4,65 +4,74 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.easeui.domain.User;
+
 import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.db.UserDao;
+import cn.ucai.superwechat.utils.L;
 
 
 /**
  * 开屏页
- *
  */
 public class SplashActivity extends BaseActivity {
+    private static final String TAG=SplashActivity.class.getSimpleName();
+    private static final int sleepTime = 2000;
+    SplashActivity mContext;
 
-	private static final int sleepTime = 2000;
+    @Override
+    protected void onCreate(Bundle arg0) {
+        setContentView(R.layout.em_activity_splash);
+        super.onCreate(arg0);
+mContext=this;
+    }
 
-	@Override
-	protected void onCreate(Bundle arg0) {
-		setContentView(R.layout.em_activity_splash);
-		super.onCreate(arg0);
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-	}
+        new Thread(new Runnable() {
+            public void run() {
+                if (SuperWeChatHelper.getInstance().isLoggedIn()) {
+                    // auto login mode, make sure all group and conversation is loaed before enter the main screen
+                    long start = System.currentTimeMillis();
+                    EMClient.getInstance().groupManager().loadAllGroups();
+                    EMClient.getInstance().chatManager().loadAllConversations();
+                    UserDao dao=new UserDao(mContext);
+                    User user = dao.getUser(EMClient.getInstance().getCurrentUser());
+                    L.e(TAG,"user="+user);
+                    SuperWeChatHelper.getInstance().setCurrentUser(user);
 
-	@Override
-	protected void onStart() {
-		super.onStart();
+                    long costTime = System.currentTimeMillis() - start;
+                    //wait
+                    if (sleepTime - costTime > 0) {
+                        try {
+                            Thread.sleep(sleepTime - costTime);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    //enter main screen
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                } else {
+                    try {
+                        Thread.sleep(sleepTime);
+                    } catch (InterruptedException e) {
+                    }
+                    startActivity(new Intent(SplashActivity.this, GuideActivity.class));
+                    finish();
+                }
+            }
+        }).start();
 
-		new Thread(new Runnable() {
-			public void run() {
-				if (SuperWeChatHelper.getInstance().isLoggedIn()) {
-					// auto login mode, make sure all group and conversation is loaed before enter the main screen
-					long start = System.currentTimeMillis();
-					EMClient.getInstance().groupManager().loadAllGroups();
-					EMClient.getInstance().chatManager().loadAllConversations();
-					long costTime = System.currentTimeMillis() - start;
-					//wait
-					if (sleepTime - costTime > 0) {
-						try {
-							Thread.sleep(sleepTime - costTime);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-					//enter main screen
-					startActivity(new Intent(SplashActivity.this, MainActivity.class));
-					finish();
-				}else {
-					try {
-						Thread.sleep(sleepTime);
-					} catch (InterruptedException e) {
-					}
-					startActivity(new Intent(SplashActivity.this, GuideActivity.class));
-					finish();
-				}
-			}
-		}).start();
+    }
 
-	}
-	
-	/**
-	 * get sdk version
-	 */
-	private String getVersion() {
-	    return EMClient.getInstance().getChatConfig().getVersion();
-	}
+    /**
+     * get sdk version
+     */
+    private String getVersion() {
+        return EMClient.getInstance().getChatConfig().getVersion();
+    }
 }
