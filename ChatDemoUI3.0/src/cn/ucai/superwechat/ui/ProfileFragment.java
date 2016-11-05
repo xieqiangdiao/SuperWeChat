41c27a8dd6785854bcfbdc26fba33277d0e108b0
xieqiangdiao <765648978@qq.com>
2016-11-05 19:26:37 +0800

    修改个人资料显示

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ProfileFragment.java
@@ -0,0 +1,103 @@
+package cn.ucai.superwechat.ui;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.easemob.redpacketui.utils.RedPacketUtil;
+import com.hyphenate.easeui.utils.EaseUserUtils;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.superwechat.Constant;
+import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.utils.MFGT;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProfileFragment extends Fragment {
+
+
+    @Bind(R.id.profile_avatar)
+    ImageView profileAvatar;
+    @Bind(R.id.profile_nick)
+    TextView profileNick;
+    @Bind(R.id.profile_weixin)
+    TextView profileWeixin;
+    @Bind(R.id.profile_setting)
+    RelativeLayout profileSetting;
+    @Bind(R.id.profile_scrollview)
+    ScrollView profileScrollview;
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null && savedInstanceState.getBoolean("isConflict", false))
+            return;
+        setUserInfo();
+    }
+
+    private void setUserInfo() {
+        EaseUserUtils.setCurrentAppUserAvatar(getActivity(), profileAvatar);
+        EaseUserUtils.setCurrentAppUserNick(profileNick);
+        EaseUserUtils.setCurrentAppUserUserNameWithNo(profileWeixin);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick({R.id.layout_profile_view, R.id.profile_money, R.id.profile_smail})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.layout_profile_view:
+                MFGT.gotoUserProfile(getActivity());
+                break;
+            //red packet code : 进入零钱页面
+            case R.id.profile_money:
+                RedPacketUtil.startChangeActivity(getActivity());
+                break;
+            //end of red packet code
+            case R.id.tv_profile_setting:
+                MFGT.gotoSettings(getActivity());
+                break;
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (((MainActivity) getActivity()).isConflict) {
+            outState.putBoolean("isConflict", true);
+        } else if (((MainActivity) getActivity()).getCurrentAccountRemoved()) {
+            outState.putBoolean(Constant.ACCOUNT_REMOVED, true);
+        }
+    }
+
+}
