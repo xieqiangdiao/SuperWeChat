c43111d0fd0a34c3811f3b562952d44fce86f6f0
xieqiangdiao <765648978@qq.com>
2016-11-05 17:17:20 +0800

    修改头像的默认图片，添加设置当前登录用户的昵称

--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -1,43 +1,55 @@
 package com.hyphenate.easeui.utils;
 
 import android.content.Context;
+import android.support.v4.app.FragmentActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.hyphenate.chat.EMClient;
 import com.hyphenate.easeui.R;
 import com.hyphenate.easeui.controller.EaseUI;
 import com.hyphenate.easeui.controller.EaseUI.EaseUserProfileProvider;
 import com.hyphenate.easeui.domain.EaseUser;
+import com.hyphenate.easeui.domain.User;
 
 public class EaseUserUtils {
-    
+
     static EaseUserProfileProvider userProvider;
-    
+
     static {
         userProvider = EaseUI.getInstance().getUserProfileProvider();
     }
-    
+
     /**
      * get EaseUser according username
+     *
      * @param username
      * @return
      */
-    public static EaseUser getUserInfo(String username){
-        if(userProvider != null)
+    public static EaseUser getUserInfo(String username) {
+        if (userProvider != null)
             return userProvider.getUser(username);
-        
+
         return null;
     }
-    
+
+    public static User getAppUserInfo(String username) {
+        if (userProvider != null)
+            return userProvider.getAppUser(username);
+
+        return null;
+    }
+
     /**
      * set user avatar
+     *
      * @param username
      */
-    public static void setUserAvatar(Context context, String username, ImageView imageView){
-    	EaseUser user = getUserInfo(username);
-        if(user != null && user.getAvatar() != null){
+    public static void setUserAvatar(Context context, String username, ImageView imageView) {
+        EaseUser user = getUserInfo(username);
+        if (user != null && user.getAvatar() != null) {
             try {
                 int avatarResId = Integer.parseInt(user.getAvatar());
                 Glide.with(context).load(avatarResId).into(imageView);
@@ -45,23 +57,78 @@ public class EaseUserUtils {
                 //use default avatar
                 Glide.with(context).load(user.getAvatar()).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.ease_default_avatar).into(imageView);
             }
-        }else{
+        } else {
             Glide.with(context).load(R.drawable.ease_default_avatar).into(imageView);
         }
     }
-    
+
     /**
      * set user's nickname
      */
-    public static void setUserNick(String username,TextView textView){
-        if(textView != null){
-        	EaseUser user = getUserInfo(username);
-        	if(user != null && user.getNick() != null){
-        		textView.setText(user.getNick());
-        	}else{
-        		textView.setText(username);
-        	}
+    public static void setUserNick(String username, TextView textView) {
+        if (textView != null) {
+            EaseUser user = getUserInfo(username);
+            if (user != null && user.getNick() != null) {
+                textView.setText(user.getNick());
+            } else {
+                textView.setText(username);
+            }
         }
     }
-    
+
+    /**
+     * set user avatar
+     *
+     * @param username
+     */
+    public static void setAppUserAvatar(Context context, String username, ImageView imageView) {
+        User user = getAppUserInfo(username);
+        if (user != null && user.getAvatar() != null) {
+            try {
+                int avatarResId = Integer.parseInt(user.getAvatar());
+                Glide.with(context).load(avatarResId).into(imageView);
+            } catch (Exception e) {
+                //use default avatar
+                Glide.with(context).load(user.getAvatar()).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.ease_default_avatar).into(imageView);
+            }
+        } else {
+            Glide.with(context).load(R.drawable.ease_default_avatar).into(imageView);
+        }
+    }
+
+    /**
+     * set app user's nickname
+     */
+    public static void setAppUserNick(String username, TextView textView) {
+        if (textView != null) {
+            User user = getAppUserInfo(username);
+            if (user != null && user.getMUserNick() != null) {
+                textView.setText(user.getMUserNick());
+            } else {
+                textView.setText(username);
+            }
+        }
+    }
+
+
+    public static void setCurrentAppUserAvatar(FragmentActivity activity, ImageView imageView) {
+        String username = EMClient.getInstance().getCurrentUser();
+        setAppUserAvatar(activity, username, imageView);
+    }
+
+    public static void setCurrentAppUserNick(TextView textView) {
+        String username = EMClient.getInstance().getCurrentUser();
+        setAppUserNick(username, textView);
+
+    }
+
+
+    public static void setCurrentAppUserUserNameWithNo(TextView textView) {
+        String username = EMClient.getInstance().getCurrentUser();
+        setAppUserName("微信号:",username, textView);
+    }
+
+    private static void setAppUserName(String suffix, String username, TextView textView) {
+        textView.setText(suffix + username);
+    }
 }
