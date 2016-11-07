83920d63a8b4109c1b490751b283f9d578d21628
xieqiangdiao <765648978@qq.com>
2016-11-07 10:52:21 +0800

    修改跟人中心的bug

--- easeui/src/com/hyphenate/easeui/domain/User.java
+++ easeui/src/com/hyphenate/easeui/domain/User.java
@@ -122,7 +122,7 @@ public class User implements Serializable {
 	}
 
 	public String getAvatar() {
-		String path="aaaaaaa";
+		String path="http://101.251.196.90:8000/SuperWeChatServerV2.0/downloadAvatar?name_or_hxid=\"+getMUserName()+\"&avatarType=user_avatar&m_avatar_suffix=\"+getMAvatarSuffix()";
 		return  path;
 	}
 }
