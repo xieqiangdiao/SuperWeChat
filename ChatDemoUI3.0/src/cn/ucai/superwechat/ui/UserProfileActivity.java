859e51fb75c6018c0512d2288c809765b737af57
xieqiangdiao <765648978@qq.com>
2016-11-07 11:22:44 +0800

    更新头像，及内存缓存

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/UserProfileActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/UserProfileActivity.java
@@ -23,13 +23,20 @@ import com.bumptech.glide.Glide;
 import com.hyphenate.EMValueCallBack;
 import com.hyphenate.easeui.domain.EaseUser;
 import com.hyphenate.easeui.domain.User;
+import com.hyphenate.easeui.utils.EaseImageUtils;
 import com.hyphenate.easeui.utils.EaseUserUtils;
 
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.bean.Result;
@@ -205,7 +212,7 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
     }
 
     private void updateLocatUser(User u) {
-        user =u;
+        user = u;
         SuperWeChatHelper.getInstance().saveAppContact(u);
         EaseUserUtils.setCurrentAppUserNick(tvNickName);
     }
@@ -221,6 +228,7 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
                 break;
             case REQUESTCODE_CUTTING:
                 if (data != null) {
+                    updateAppUserAvatar(data);
                     setPicToView(data);
                 }
                 break;
@@ -228,6 +236,60 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
                 break;
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void updateAppUserAvatar(final Intent picData) {
+        dialog = ProgressDialog.show(this, getString(R.string.dl_update_photo), getString(R.string.dl_waiting));
+        dialog.show();
+        File file = saveBitmapFile(picData);
+        NetDao.updateAvatar(this, user.getMUserName(), file, new OkHttpUtils.OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                if (s != null) {
+                    Result result = ResultUtils.getResultFromJson(s, User.class);
+                    L.e(TAG, "result=" + result);
+                    if (result != null && result.isRetMsg()) {
+                        User u = (User) result.isRetData();
+                        SuperWeChatHelper.getInstance().saveAppContact(u);
+                        setPicToView(picData);
+                    } else {
+                        dialog.dismiss();
+                        CommonUtils.showMsgShortToast(result != null ? result.getRetCode() : -1);
+                        //CommonUtils.showMsgShortToast(result!=null?result.getRetCode():R.string.toast_updatephoto_fail);
+                    }
+                } else {
+                    dialog.dismiss();
+                    CommonUtils.showShortToast(R.string.toast_updatephoto_fail);
+                }
+
+            }
+
+            @Override
+            public void onError(String error) {
+                L.e(TAG, "error=" + error);
+                CommonUtils.showShortToast(R.string.toast_updatephoto_fail);
+            }
+        });
+    }
+
+    private File saveBitmapFile(Intent picData) {
+        Bundle extras = picData.getExtras();
+        if (extras != null) {
+            Bitmap bitmap = extras.getParcelable("data");
+            String imagePath = EaseImageUtils.getImagePath(user.getMUserName() + I.AVATAR_SUFFIX_JPG);
+            File file = new File(imagePath);//将要保存图片的路径
+            L.e("file path=" + file.getAbsolutePath());
+            try {
+                BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, bos);
+                bos.flush();
+                bos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return file;
+        }
+        return null;
     }
 
     public void startPhotoZoom(Uri uri) {
@@ -302,6 +364,7 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
                 break;
 
             case R.id.user_head_avatar:
+                L.e(TAG, "user.getAvatar()=" + user.getAvatar());
                 uploadHeadPhoto();
                 break;
             case R.id.rl_nickname:
