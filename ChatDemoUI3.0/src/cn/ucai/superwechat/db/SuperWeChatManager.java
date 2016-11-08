6fc4ac9f84fc58e15d43429f371ae793a3d3c880
xieqiangdiao <765648978@qq.com>
2016-11-08 14:45:23 +0800

    新的朋友处理按时间倒序排列

--- ChatDemoUI3.0/src/cn/ucai/superwechat/db/SuperWeChatManager.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/db/SuperWeChatManager.java
@@ -227,7 +227,7 @@ public class SuperWeChatManager {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         List<InviteMessage> msgs = new ArrayList<InviteMessage>();
         if (db.isOpen()) {
-            Cursor cursor = db.rawQuery("select * from " + InviteMessgeDao.TABLE_NAME + " desc", null);
+            Cursor cursor = db.rawQuery("select * from " + InviteMessgeDao.TABLE_NAME + " order by"+InviteMessgeDao.COLUMN_NAME_TIME+"desc", null);
             while (cursor.moveToNext()) {
                 InviteMessage msg = new InviteMessage();
                 int id = cursor.getInt(cursor.getColumnIndex(InviteMessgeDao.COLUMN_NAME_ID));
